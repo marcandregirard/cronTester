683f28b83317bc133ba94fe7037842923b7946b0
marcandregirard <marcandr.girard@gmail.com>
2014-09-17 11:51:23 -0400

    extracted the creation of dates in his method.
    Preparing the creation of a Class to create the output. Separating the concern of outputting to create the data.

--- src/cronTester/CronTester.java
+++ src/cronTester/CronTester.java
@@ -1,7 +1,9 @@
 package cronTester;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.quartz.CronExpression;
 
@@ -9,19 +11,27 @@ public class CronTester {
 
 	/**
 	 * @param args
-	 * @throws ParseException 
+	 * @throws ParseException
 	 */
 	public static void main(String[] args) throws ParseException {
-        final String expression = args[0];
-	    final int numberOfDates = Integer.parseInt(args[1]);    
-        final CronExpression cronExpression = new CronExpression(expression);
-        Date nextValidDate = cronExpression.getNextValidTimeAfter(new Date());
-        for(int i = 0;i < numberOfDates; i++) {
-        	System.out.println(nextValidDate);       	
-        	nextValidDate = cronExpression.getNextValidTimeAfter(nextValidDate);
-        }
- 
+		final String expression = args[0];
+		final int numberOfDates = Integer.parseInt(args[1]);
+		final CronExpression cronExpression = new CronExpression(expression);
+		List<Date> result = createValidTimeDates(cronExpression,numberOfDates);
+		for(Date expected : result) {
+			System.out.println(expected);
+		}
 
+	}
+	
+	private static List<Date> createValidTimeDates(CronExpression cron, int numberOfDates) { 
+		Date nextValidDate = cron.getNextValidTimeAfter(new Date());
+		List<Date> results = new ArrayList<>();
+		for (int i = 0; i < numberOfDates; i++) {
+			results.add(nextValidDate);
+			nextValidDate = cron.getNextValidTimeAfter(nextValidDate);
+		}
+		return results;
 	}
 
 }
